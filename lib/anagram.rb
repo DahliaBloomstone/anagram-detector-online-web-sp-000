ca9178ee493cdc7f1bfd9965632ce4ca5b44e1ca
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-27 21:04:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,3 +5,11 @@ attr_accessor :word
 def initialize(word)
 @word = word
 end
+
+def match(word_array)
+  word_array.select do |word|
+    word.split("").sort ==
+    @word.split("").sort
+end
+end
+end  
